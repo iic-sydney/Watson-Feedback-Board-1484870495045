6a4c8c8cb28b6c06d6b2230e95901306107ea673
IBM Innovation Centre <iic@vuejs-dashboard.syd.iic.ihost.com>
2017-01-20 11:30:18 +1100

    Initial Migration

--- app.js
+++ app.js
@@ -7,16 +7,28 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var path = require('path')
+var bodyParser = require('body-parser')
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+var index = require('./routes/index')
+
 // create a new express server
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false });
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+app.use('/', index)
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
