a4fb2bc4eee5f7cd46c5cfcb94d2ea6495b8f985
IBM Innovation Centre <iic@vuejs-dashboard.syd.iic.ihost.com>
2017-01-20 11:36:08 +1100

    rolled back app.js

--- app.js
+++ app.js
@@ -14,21 +14,17 @@ var bodyParser = require('body-parser')
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-var index = require('./routes/index')
-
 // create a new express server
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false });
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
-app.use('/', index)
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
