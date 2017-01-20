d9daddca4155f98c089f7e3ba917116614f230dd
IBM Innovation Centre <iic@vuejs-dashboard.syd.iic.ihost.com>
2017-01-20 11:45:17 +1100

    fixed a typo

--- app.js
+++ app.js
@@ -7,7 +7,6 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-var path = require('path')
 var bodyParser = require('body-parser')
 
 // cfenv provides access to your Cloud Foundry environment
@@ -21,7 +20,7 @@ var app = express();
 //app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
